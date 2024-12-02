52abedcd00b047fe5ccf953457758feb0b8da3a3
yousef9 <yousef9@DESKTOP-D2NIA5M>
2024-12-02 04:00:40 +0200

    Add Date to screens and Display User

--- Banking-System/ClsScreen.h
+++ Banking-System/ClsScreen.h
@@ -7,21 +7,23 @@ using namespace std;
 class ClsScreen
 {
 protected:
-	static void _DrawScreenHeader(string Title,string SubTitle="",bool IsNotLoginScreen=true) {
-		
+	static void _DrawScreenHeader(string Title, string SubTitle = "", bool IsNotLoginScreen = true) {
+		ClsDate Date;
 			
 		cout << "\t\t\t\t______________________________________\n\n";
 		cout << "\t\t\t\t"<< Title << endl;
-		if (IsNotLoginScreen)
-		{
-
-		cout << "\n User :  "<< CurrentUser.UserName << endl;
-		}
 		if (SubTitle!="")
 		{
 			cout << "\n\t\t\t\t" << SubTitle;
 		}
 		cout << "\n\t\t\t\t______________________________________\n\n";
+		if (IsNotLoginScreen)
+		{
+
+		cout << "\t\t\t\tUser:"<< CurrentUser.UserName << endl;
+		}
+		cout << "\t\t\t\tDate:";
+			Date.print() ;
 		
 		
 		
